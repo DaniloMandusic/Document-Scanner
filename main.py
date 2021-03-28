90dbadb41b6fd6d340f687bdbd2e86736454aaff
danilo <danilo.mandusic1@gmail.com>
2021-03-28 20:44:57 +0200

    added pdf converter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import img2pdf as img2pdf
 import numpy as np
 import pytesseract as pytesseract
 
@@ -54,3 +55,10 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Python38\\Tesseract\\tesseract.exe'
 
 print(pytesseract.image_to_string(dst))
 cv2.imwrite('Test_dst.jpg', dst)
+
+string = pytesseract.image_to_string(dst)
+
+pdf = img2pdf.convert("Test_dst.jpg")
+file = open("newfile.pdf","wb")
+file.write(pdf)
+file.close()
