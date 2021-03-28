8ddc70806d4f406ffd8074563132c5d7ab935a8f
danilo <danilo.mandusic1@gmail.com>
2021-03-28 14:41:07 +0200

    added basic scanner

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import mapper
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#name your picture
+image = cv2.imread('1.jpg')
+image = cv2.resize(image, (1500, 880))
+
+# creating copy of original image
+orig = image.copy()
+
+# convert to gray and blur
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+#detect edges
+edged = cv2.Canny(blurred, 0, 50)
+orig_edged = edged.copy()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+(contours, _) = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+#x,y,w,h = cv2.boundingRect(contours[0])
+#cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),0)
+
+# get approximate contour
+for c in contours:
+    p = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * p, True)
+
+    if len(approx) == 4:
+        target = approx
+        break
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# mapping target points to 800x800 quadrilateral
+approx = mapper.mapp(target)
+pts2 = np.float32([[0,0],[800,0],[800,800],[0,800]])
 
+M = cv2.getPerspectiveTransform(approx,pts2)
+dst = cv2.warpPerspective(orig,M,(800,800))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+cv2.drawContours(image, [target], -1, (0, 255, 0), 2)
+dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cv2.imshow("dst.jpg", dst)
+cv2.waitKey(0)
